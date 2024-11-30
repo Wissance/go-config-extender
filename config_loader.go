7c8d1c23a7bf78f8bb1aea2ba8538e537f467181
Ushakov Michale <um.nix.user@gmail.com>
2024-11-30 21:41:28 +0500

    config loader: 1 method

--- config_loader.go
+++ config_loader.go
@@ -1,9 +1,30 @@
 package go_config_extender
 
-func LoadConfig[T any](file string) (T, error) {
-	return nil, nil
+import (
+	"encoding/json"
+	"errors"
+	sf "github.com/wissance/stringFormatter"
+	"os"
+	"path/filepath"
+)
+
+func LoadJSONConfig[T any](configFile string) (T, error) {
+	absPath, err := filepath.Abs(configFile)
+	if err != nil {
+		return nil, errors.New(sf.Format("an error occurred during getting config file abs path: {0}", err.Error()))
+	}
+
+	fileData, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, errors.New(sf.Format("an error occurred during config file reading: {0}", err.Error()))
+	}
+	var cfg T
+	if err = json.Unmarshal(fileData, &cfg); err != nil {
+		return nil, errors.New(sf.Format("an error occurred during config file unmarshal:  {0}", err.Error()))
+	}
+	return cfg, nil
 }
 
-func LoadConfigWithEnv[T any](file string) (T, error) {
+func LoadJSONConfigWithEnvOverride[T any](file string) (T, error) {
 	return nil, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wissance/go-config-extender
 
 go 1.19
+
+require github.com/wissance/stringFormatter v1.3.0
