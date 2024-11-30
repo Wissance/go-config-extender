c27a8e70e5c5d68dc9c22908a9a28a857f60f52c
Ushakov Michale <um.nix.user@gmail.com>
2024-11-30 22:18:17 +0500

    Reading JSON vars

--- config_loader.go
+++ config_loader.go
@@ -6,9 +6,44 @@ import (
 	sf "github.com/wissance/stringFormatter"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+const technicalEnvPrefix = "__"
+
 func LoadJSONConfig[T any](configFile string) (T, error) {
+	fileData, err := readJSONConfigStr(configFile)
+	var cfg T
+	if err = json.Unmarshal(fileData, &cfg); err != nil {
+		return nil, errors.New(sf.Format("an error occurred during config file unmarshal:  {0}", err.Error()))
+	}
+	return cfg, nil
+}
+
+func LoadJSONConfigWithEnvOverride[T any](configFile string) (T, error) {
+	fileData, err := readJSONConfigStr(configFile)
+	if err != nil {
+		return nil, err
+	}
+	var rawCfg map[string]interface{}
+	if err = json.Unmarshal(fileData, &rawCfg); err != nil {
+		return nil, errors.New(sf.Format("an error occurred during config file unmarshal:  {0}", err.Error()))
+	}
+	allEnvVars := os.Environ()
+	var techEnvVars = map[string]string{}
+	for _, pair := range allEnvVars {
+		// 1. Pair Key=Value should start from __
+		if strings.HasPrefix(pair, technicalEnvPrefix) {
+			// 2. Split match pair by =
+			parts := strings.Split(pair, "=")
+			envVarPath := strings.TrimPrefix(parts[0], technicalEnvPrefix)
+			techEnvVars[envVarPath] = parts[1]
+		}
+	}
+	return nil, nil
+}
+
+func readJSONConfigStr(configFile string) ([]byte, error) {
 	absPath, err := filepath.Abs(configFile)
 	if err != nil {
 		return nil, errors.New(sf.Format("an error occurred during getting config file abs path: {0}", err.Error()))
@@ -18,13 +53,6 @@ func LoadJSONConfig[T any](configFile string) (T, error) {
 	if err != nil {
 		return nil, errors.New(sf.Format("an error occurred during config file reading: {0}", err.Error()))
 	}
-	var cfg T
-	if err = json.Unmarshal(fileData, &cfg); err != nil {
-		return nil, errors.New(sf.Format("an error occurred during config file unmarshal:  {0}", err.Error()))
-	}
-	return cfg, nil
-}
 
-func LoadJSONConfigWithEnvOverride[T any](file string) (T, error) {
-	return nil, nil
+	return fileData, nil
 }
